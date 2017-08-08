f07f50d72b0294931cd70ce9c2e7319023d3c4b0
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-08-08 08:20:29 +0300

    Added save to file option

--- src/com/znow/zetlogger/ZetLogger.java
+++ src/com/znow/zetlogger/ZetLogger.java
@@ -1,5 +1,8 @@
 package com.znow.zetlogger;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,7 +25,11 @@ public class ZetLogger {
 		String logEntry = dateFormat.format(date) + "  INFO     " + className + " - " + info;
 		
 		if (configurator.isConsoleOutputEnabled()) {
-			System.out.println(dateFormat.format(date) + "  INFO     " + className + " - " + info);
+			System.out.println(logEntry);
+		}
+		
+		if (configurator.isSaveToFileEnabled()) {
+			saveToFile(configurator.getLogFilePath(), logEntry);
 		}
 	}
 	
@@ -33,7 +40,11 @@ public class ZetLogger {
 		String logEntry = dateFormat.format(date) + "  TRACE    " + className + " - " + info;
 		
 		if (configurator.isConsoleOutputEnabled()) {
-			System.out.println(dateFormat.format(date) + "  TRACE    " + className + " - " + info);
+			System.out.println(logEntry);
+		}
+		
+		if (configurator.isSaveToFileEnabled()) {
+			saveToFile(configurator.getLogFilePath(), logEntry);
 		}
 	}
 	
@@ -44,7 +55,11 @@ public class ZetLogger {
 		String logEntry = dateFormat.format(date) + "  WARNING  " + className + " - " + info;
 		
 		if (configurator.isConsoleOutputEnabled()) {
-			System.out.println(dateFormat.format(date) + "  WARNING  " + className + " - " + info);
+			System.out.println(logEntry);
+		}
+		
+		if (configurator.isSaveToFileEnabled()) {
+			saveToFile(configurator.getLogFilePath(), logEntry);
 		}
 	}
 	
@@ -55,12 +70,38 @@ public class ZetLogger {
 		String logEntry = dateFormat.format(date) + "  ERROR    " + className + " - " + info;
 		
 		if (configurator.isConsoleOutputEnabled()) {
-			System.out.println(dateFormat.format(date) + "  ERROR    " + className + " - " + info);
+			System.out.println(logEntry);
+		}
+		
+		if (configurator.isSaveToFileEnabled()) {
+			saveToFile(configurator.getLogFilePath(), logEntry);
 		}
 	}
 	
-	private void saveToFile(String filePath) {
+	private void saveToFile(String filePath, String logEntry) {
+		FileWriter fw = null;
+		BufferedWriter writer = null;
 		
+		try {
+			fw = new FileWriter(filePath, true);
+			
+			writer = new BufferedWriter(fw);
+			
+			writer.write(logEntry);
+			writer.newLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+				try {
+					if (writer != null)
+						writer.close();
+					
+					if (fw != null)
+						fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
 	}
 	
 }
